d44a27e5b0c666d0f85a3bf5bdac35d611504efd
aniruddha <aniruddha.kudalkar@gmail.com>
2017-07-01 13:14:18 +0530

    router :  route variable observable

--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -1,22 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-second',
   templateUrl: './second.component.html',
   styleUrls: ['./second.component.css']
 })
-export class SecondComponent implements OnInit {
+export class SecondComponent implements OnInit, OnDestroy {
 
   usNm: string;
+  private sub: Subscription;
 
   constructor(
+    private router: Router,
     private activatedRoute: ActivatedRoute
   ) {
 
   }
 
   ngOnInit() {
-    this.usNm = this.activatedRoute.snapshot.params['usNm'];
+    //this.usNm = this.activatedRoute.snapshot.params['usNm'];
+    this.activatedRoute.params.subscribe(prms => this.usNm = prms['usNm']);
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  changeUsNm(usNm: string) {
+    this.router.navigate(['second', usNm]);
   }
 }
